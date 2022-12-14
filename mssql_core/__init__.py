4ebb9cb17455df0c88d5e7708eb0635f9a12b380
John Omernik <darkness@cybermac.local>
2022-12-14 07:42:40 -0600

    Update to lazy load

--- mssql_core/__init__.py
+++ mssql_core/__init__.py
@@ -1,4 +1,4 @@
-from integration_core import Integration
-from pyodbc_core.pyodbc_base import Pyodbc
+#from integration_core import Integration
+#from pyodbc_core.pyodbc_base import Pyodbc
 from mssql_core.mssql_base import Mssql
 from mssql_core._version import __version__
